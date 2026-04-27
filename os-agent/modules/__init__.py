144f8f8ab39b8a1dcfa3cf496afaa3b46b18c689
hohomewen-cyber <hohomewen@gmail.com>
2026-04-27 16:47:36 +0800

    Update __init__.py

--- os-agent/modules/__init__.py
+++ os-agent/modules/__init__.py
@@ -1,11 +1,11 @@
 from .c_executor import CExecutor, c_executor
 from .document_parser import DocumentParser
-# from .windows_terminal import WindowsTerminal, terminal
+from .windows_terminal import WindowsTerminal, terminal
 
 __all__ = [
     "CExecutor",
     "DocumentParser",
-    # "WindowsTerminal",
+    "WindowsTerminal",
     "c_executor",
     "terminal",
 ]
